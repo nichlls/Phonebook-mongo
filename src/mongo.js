52ef0075aa42cea2edd7f3387eff0334222b2a4b
nichlls <107227159+nichlls@users.noreply.github.com>
2024-08-16 18:14:58 +0100

    3.12

---
+++ src/mongo.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://nichlls:${password}@phonebook.ijjuz.mongodb.net/?retryWrites=true&w=majority&appName=phonebook`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((persons) => {
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log(`Added ${person.name} ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
